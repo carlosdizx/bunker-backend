8125b3223d0b6bc457da53ff4e821110bc151af0
carlosdizx <carlodiaz@umariana.edu.co>
2021-06-17 23:23:59 -0500

    Full los metodos de listar, buscar y crear 😎👍

--- src/main/java/bunker/controllers/RestController.java
+++ src/main/java/bunker/controllers/RestController.java
@@ -1,27 +1,79 @@
 package bunker.controllers;
 
+import bunker.entidad.Person;
 import bunker.service.api.IPersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = {"http://localhost:8080"})
+@CrossOrigin(origins = {"*"})
 @org.springframework.web.bind.annotation.RestController
-@RequestMapping("/api")
+@RequestMapping("/api/")
 public class RestController
 {
+    //--------------------------- CONST ---------------------------
     final static Map<String, Object> RESPONSE = new HashMap<>();
+
+    //--------------------------- Attributes ---------------------------
 
     @Autowired
     private IPersonService personService;
 
-    @GetMapping("/person/all")
-    public List<?> getAllPersons() {
+    //--------------------------- for Person ---------------------------
+
+    @GetMapping("person/all")
+    public List<Person> getAllPersons()
+    {
         return personService.getAll();
     }
+    
+    @GetMapping("person/get/{id}")
+    public ResponseEntity<Map<String, Object>> findPersonById(@PathVariable Integer id)
+    {
+        RESPONSE.clear();
+        try
+        {
+            final Person person = personService.findByID(id);
+            if (person == null)
+            {
+                RESPONSE.put("Mensaje", "Persona no encontrada");
+                return new ResponseEntity<>(RESPONSE, HttpStatus.NOT_FOUND);
+            }
+            RESPONSE.put("Persona", person);
+            return new ResponseEntity<>(RESPONSE, HttpStatus.OK);
+        }
+        catch (DataAccessException e)
+        {
+            RESPONSE.put("Mensaje", "No se ha logrado realizar la consulta en la base de datos");
+            RESPONSE.put("Error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity(RESPONSE, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("person/all")
+    public ResponseEntity<Map<String, Object>> savePerson(@RequestBody Person pPerson)
+    {
+        System.out.println("---->"+pPerson);
+        System.out.println("---->"+pPerson);
+        System.out.println("---->"+pPerson);
+        System.out.println("---->"+pPerson);
+        final Person person = personService.save(pPerson);
+        if (person == null)
+        {
+            RESPONSE.put("Mensaje", "No se pudo agregar a la persona");
+            return new ResponseEntity<>(RESPONSE, HttpStatus.NOT_FOUND);
+        }
+        RESPONSE.put("Mensaje", "Se agrego a la persona con exito!");
+        return new ResponseEntity<>(RESPONSE, HttpStatus.OK);
+    }
+
+    //--------------------------- for Product ---------------------------
+
+    //--------------------------- for Sale ---------------------------
 }
--- src/main/java/bunker/entidad/Person.java
+++ src/main/java/bunker/entidad/Person.java
@@ -1,5 +1,7 @@
 package bunker.entidad;
 
+import org.hibernate.annotations.CreationTimestamp;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
@@ -22,5 +24,67 @@ public class Person implements Serializable {
 
     private String email;
 
+    @CreationTimestamp
     private Date created;
+
+    public Person()
+    {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
 }
